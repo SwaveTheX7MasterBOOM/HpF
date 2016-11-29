4718f5e277dfc90f2b1abffb56c4620711ea8465
SwaveTheX7MasterBOOM <thomas.capach@gmail.com>
2016-11-29 15:13:27 -0500

    testing

--- LoginActivity.java
+++ LoginActivity.java
@@ -359,6 +359,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
     }
 
+
     @Override
     public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor)
     {
